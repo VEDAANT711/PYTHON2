71c8fa215db7de3fbd25865b57c52691d1b14ccf
VEDANT711 <gaikwadvedant2@gmail.com>
2024-04-20 13:41:08 +0530

    added

--- FileHandling4.py
+++ FileHandling4.py
@@ -1,17 +1,56 @@
-reclen = 10
+# reclen = 10
+# with open('cities.bin','wb') as f:
+#     n = int(input('ENTER THE NUMBER OF CITIES='))
+#     for x in range(n):
+#         city = input ('ENTER THE CITY=')
+#         city = city + (reclen - len(city)) * " " 
+#         city = city.encode()
+#         f.write(city)
+
+reclen = 20
 with open('cities.bin','wb') as f:
-    n = int(input('ENTER THE NUMBER OF CITIES='))
+    n = int(input('Enter the number of cities: '))
     for x in range(n):
-        city = input ('ENTER THE CITY=')
-        city = city + (reclen - len(city)) * " " 
+        city = input('enter the city: ') # pune
+        city = city + (reclen - len(city)) * " "
         city = city.encode()
         f.write(city)
 
+
 # reclen = 20
-# with open('cities.bin','wb') as f:
-#     n = int(input('Enter the number of cities'))
-#     for x in range(n):
-#         city = input('enter the city') # pune
-#         city = city + (reclen - len(city)) * " "
-#         city = city.encode()
-#         f.write(city)
+# try:
+#     with open('cities.bin','rb') as f:
+#         n = int(input('record number'))
+#         f.seek(reclen * (n-1))
+#         str = f.read(reclen)
+#         str = str.decode()
+#         print(str)
+# except FileNotFoundError:
+#     print("File not found")
+
+
+
+    
+
+reclen = 20
+try:
+    with open('cities.bin', 'rb') as f:
+        n = int(input('Enter the record number: '))
+        f.seek(reclen * (n - 1))
+        data = f.read(reclen)
+        city = data.decode().strip()  # Remove any trailing whitespace
+        print("City:", city)
+except FileNotFoundError:
+    print("File not found.")
+except ValueError:
+    print("Invalid record number. Please")
+
+
+
+# reclen = 20
+# with open('cities.bin','rb') as f:
+#     n = int(input('record number')) # 1
+#     f.seek(reclen * (n-1))
+#     str = f.read(reclen)
+#     str = str.decode().strip()
+#     print("str:",str)
