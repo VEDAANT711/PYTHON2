c31c08dff000eebab33b0292fb6a10e284b55666
VEDANT711 <gaikwadvedant2@gmail.com>
2024-05-28 21:56:55 +0530

    added

---
+++ new_practise_2.py
@@ -0,0 +1,64 @@
+# g = 10
+# t = 51
+
+
+# if g > t:
+#     print("g is greater")
+# else:
+#     print("t is greater") 
+
+
+# print("g is greater") if g > t else print ("t is greater")
+
+
+# age = 18
+# print("can drive ") if age >= 18 else print("cannot drive")
+
+
+# for x in range(1,11,2):
+#     print(x)
+
+# for x in range(2,21,2):
+#     print(x)    
+
+
+# for x in range (20,2,-2):
+
+#     print(x) 
+
+# for i in range (50,4,-5):
+#     print(i)
+
+# for j in range(30,2,-3):
+#     print(j)
+
+
+# for a in range (1,11):
+#     print(a)
+#     if a==3:
+#         break
+#         print(a)  
+
+# for i in range(2,21,2):
+#     print(i)
+#     if i == 14:
+#         break
+
+
+# for i in range(1,5):
+#     if i == 3:
+#         continue
+#     print(i)    
+
+
+# for i in range (1,10):
+#     if i == 6:
+#         print(i)
+#         continue
+
+for a in range(1,11):
+    print(a) # 1 # 2 #3 #4 #5
+    if a == 5:
+        break
+    
+
