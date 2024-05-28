efd06aaaad4062d6e1c2e5490f56bbd8836df3b6
VEDANT711 <gaikwadvedant2@gmail.com>
2024-05-28 22:38:41 +0530

    added

---
+++ new_practise_3.py
@@ -0,0 +1,67 @@
+names = ["ram","sham","ghansham","vedant"]
+print(names)
+print(names[0])
+print(type(names))
+
+
+names.append("amol")
+print(names)
+
+names[3]="gippy"
+print(names)
+
+names.pop()
+print(names)
+
+names.pop(1)
+print(names)
+
+f = ["chikoo","banana","apple","orange"]
+print(f[0])
+print(type(f))
+
+f.append("kiwi")
+print(f)
+
+v = ["aloo","tamatar","bhindi","meethi"]
+print("aloo" in v)
+
+if ("ladyfinger" in v ):
+    print("available")
+else:
+    print("meethi availabe instead of bhindi")    
+
+
+city = ["pondicherry","patna","thane","lucknow"]
+for x in range(4):
+    print(city [x])
+
+ 
+
+country = ["Africa","Azerbaijan","Germany","south-korea"]
+for x in range(x):
+    print(country[x]) 
+
+for i in country:
+    print(country)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
