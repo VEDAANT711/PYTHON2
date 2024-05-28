c57469739f6e0fd9ce2e8b57b9a81aa3f4ea2618
VEDANT711 <gaikwadvedant2@gmail.com>
2024-05-28 19:46:12 +0530

    added

---
+++ new_practise_1.py
@@ -0,0 +1,35 @@
+# numT = 2
+# if numT > 0 and numT <=5:
+#     print("10% discount")
+# elif numT > 5 and numT <= 10:
+#     print("15% Discount")  
+# elif numT > 10:
+#     print("30 % discount")
+# else:
+#     print('please provide correct input')
+
+
+
+marks = 78
+if marks > 90:
+    print("Grade A")
+elif marks >= 75:
+    print("Grade B")
+elif marks > 35:
+    print("Grade C")
+else:
+    print("Fail")
+
+    
+
+
+a = 101
+b = 20
+c = 30
+
+if a>b and a>c:
+    print("a is the greatest")
+elif b>a and b>c:
+    print("b is greatest")
+else:
+    print("c is greatest")       
---
+++ practice.py
@@ -0,0 +1,17 @@
+lst = [1,2,6,"w","e","r","s",7,9]
+alpha=[]
+num=[]
+for x in lst:
+   if str(x).isalpha():
+    alpha.append(x)
+   else:
+     num.append(x)
+print(alpha) 
+
+print(num)    
+
+
+     
+
+
+
