7a085a283020628fa64c6547d4e406df866dd9dc
VEDANT711 <gaikwadvedant2@gmail.com>
2024-06-01 13:34:26 +0530

    added

---
+++ new_practise_7.py
@@ -0,0 +1,14 @@
+n = "vedant"
+n1 = n.capitalize()
+print(n1)
+
+
+l = "gaikwad"
+l1 = l.startswith("ga")
+l2 = l.endswith("Ad")
+print(l1,l2)
+
+
+i = "learning TensorFlow"
+i1 = i.replace('TensorFlow','Scitic')
+print(i1)
