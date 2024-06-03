a8c88ca7f2506cb8d1f0f26eb101618be42e77b8
VEDANT711 <gaikwadvedant2@gmail.com>
2024-06-03 08:17:19 +0530

    added

---
+++ new_practise_8.py
@@ -0,0 +1,57 @@
+fn = "vedant"
+q1 = fn.upper()
+print(q1)
+
+ln = "GAIKWAD"
+q2 = ln.lower()
+print(q2)
+
+nn = "shubh"
+q3 = nn.capitalize()
+print(q3)
+
+city = "navimumbai"
+q4 = city.isupper()
+print(q4)
+
+info = "NAGPUR"
+q5 = info.islower()
+print(q5)
+
+
+info = "234211nj1n41j4n"
+q6 = info.isdigit()
+print(q6)
+
+
+info1 = "234211nj1n41j4n"
+q7 = info1.isalnum()
+print(q7)
+
+
+fruit = "apple"
+q11 = fruit.endswith('le')
+q12 = fruit.endswith('e')
+q13 = fruit.startswith('a')
+q14 = fruit.startswith('ap')
+print(q11)
+print(q12)
+print(q13)
+print(q14)
+
+
+str = " goa "
+print(len(str))
+q17 = str.strip()
+print(q17)
+print(len(q17))
+
+first_name = "vedant"
+print(first_name[1::])
+print(first_name[1:len(first_name):])
+print(first_name[0:6:2])
+print(first_name[1:-1:1])
+print(first_name[-5:-1])
+print(first_name[-1:-7:-2])
+print(first_name[::-1])
+
